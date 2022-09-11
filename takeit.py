3516461b0644bbe3ba93480eca93ca6fb1cad65f
Ferenc Géczi <ferenc.gm@gmail.com>
2022-09-11 00:00:00 +0000

    feat: Find and use an available driver
    Signed-off-by: Ferenc Géczi <ferenc.gm@gmail.com>

--- takeit.py
+++ takeit.py
@@ -3,24 +3,25 @@
 # SPDX-License-Identifier: MIT
 
 import getpass
+import os
 from urllib.parse import urlparse
 
 from selenium import webdriver
 from selenium.common.exceptions import TimeoutException
-from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.ui import WebDriverWait
 
 
 class ZendeskAuto:
-    def __init__(self, **args):
+    def __init__(self, driver_type, **args):
+        self.driver = driver_type()
         self.e_mail = args["e_mail"]
         self.password = args["password"]
         self.ticket_url = args["ticket_url"]
         parsed_result = urlparse(self.ticket_url)
         self.base_url = parsed_result.scheme + "://" + parsed_result.netloc
-        self.driver = webdriver.Firefox()
 
     def login(self):
         self.driver.get(self.base_url + "/access/normal")
@@ -66,13 +67,44 @@ class ZendeskAuto:
         submit_button.click()
 
 
+def find_driver():
+    driver_map = {"geckodriver": webdriver.Firefox, "chromedriver": webdriver.Chrome}
+
+    def find_driver_impl():
+        for pe in os.environ["PATH"].split(os.pathsep):
+            for binary_name, driver_type in driver_map.items():
+                checkname = os.path.join(pe, binary_name)
+                if os.access(checkname, os.X_OK) and not os.path.isdir(checkname):
+                    return driver_type
+
+    driver = find_driver_impl()
+    if not driver:
+        msg = (
+            "Could not find any webdriver in your system PATH.\n"
+            "Make sure you install one of those for your preferred browser:\n"
+            "https://www.selenium.dev/documentation/"
+            "webdriver/getting_started/install_drivers/"
+        )
+        raise RuntimeError(msg)
+    return driver
+
+
 if __name__ == "__main__":
 
-    zd = ZendeskAuto(
-        e_mail=input("E-Mail:\n> "),
-        password=getpass.getpass("Password (will be hidden):\n> "),
-        ticket_url=input("Ticket URL:\n> "),
-    )
-    zd.login()
-    zd.navigate_to_ticket()
-    zd.take_it_and_submit()
+    try:
+        driver_type = find_driver()
+
+        zd = ZendeskAuto(
+            driver_type=driver_type,
+            e_mail=input("E-Mail:\n> "),
+            password=getpass.getpass("Password (will be hidden):\n> "),
+            ticket_url=input("Ticket URL:\n> "),
+        )
+        zd.login()
+        zd.navigate_to_ticket()
+        zd.take_it_and_submit()
+    except Exception as e:
+        if hasattr(e, "message"):
+            print(e.message)
+        else:
+            print(e)
